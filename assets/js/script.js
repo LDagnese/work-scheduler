aeb10c56a2e6356dd5d0743676f7ec2ebf3bb47d
loudagnese <louis.dagnese@gmail.com>
2022-06-05 19:45:08 -0700

    added open-iconic link and added in icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
+// Array of objects to hold tasks. Object Properties - Date; Time; Description
+var tasks = []
+
+// createTask method
+
+// loadTask method
+
+// saveTask method
+
+// auditTask method
+
 // Display the Current Date in the Jumbotron <p> Element
 $("#currentDay").text(`Today is ${moment().format("LL")}`)
