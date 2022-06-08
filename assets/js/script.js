abe279772d8d8825d4b5b21e06c6926e51846068
loudagnese <louis.dagnese@gmail.com>
2022-06-07 23:27:19 -0700

    add save task on click, load from localstorage and save to localstorage when click saveBtn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,85 @@
 // Array of objects to hold tasks. Object Properties - Date; Time; Description
-var tasks = []
+var taskScheduler = [];
 
-// createTask method
+// get today in moment day format
+var today = moment().format("YYYYMMDD");
 
 // loadTask method
+var loadTasks = function () {
+    // get tasks from localStorage
+    taskScheduler = JSON.parse(localStorage.getItem("taskScheduler"));
+
+    if (!taskScheduler) {
+        taskScheduler = []
+        for (let i = 8; i < 18; i++) {
+            var tempObj = {
+                id: i,
+                text: ''
+            }
+            taskScheduler.push(tempObj)
+        }
+        localStorage.setItem("taskScheduler",JSON.stringify(taskScheduler))
+    } else {
+        for (let i = 0; i < taskScheduler.length; i++) {
+            $(`#${taskScheduler[i].id}.time-block p`).text(taskScheduler[i].text);
+        };
+    };
+};
 
 // saveTask method
+var saveTask = function (taskTimeID, taskText) {
+    localStorage.setItem("taskScheduler", JSON.stringify(taskScheduler))
+}
 
 // auditTask method
 
 // Display the Current Date in the Jumbotron <p> Element
 $("#currentDay").text(`Today is ${moment().format("LL")}`)
+
+// on click event for time-blocks to change to input
+$(".time-block").on("click","p",function () {
+    // get the current text of the area
+    var text = $(this)
+        .text()
+        .trim();
+    
+    // get the ID
+    var timeID = $(this).parent().attr("id");
+    console.log(timeID);
+
+    // replace p element with a new textarea
+    var textInput = $("<textarea>").addClass("col-6 descripton").val(text);
+    $(this).replaceWith(textInput);
+
+    // focus on the new element
+    textInput.trigger("focus");
+});
+
+// on click of save button run saveTask
+$(".saveBtn").on("click",function (){
+    var inputText = $(this)
+        .prev()
+        .val();
+    
+    var id = $(this)
+        .parent()
+        .attr("id");
+
+    taskScheduler.forEach(function(obj) {
+        if (obj.id == id) {
+            obj.text = inputText;
+        }
+    })
+
+    saveTask();
+
+    // replace textarea with p element
+    var pEl = $("<p>").addClass("col-6 descripton")
+    $(this).prev().replaceWith(pEl)
+
+    loadTasks();
+
+})
+
+loadTasks();
+
