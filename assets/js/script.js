2a401a1ace769eb2426a6d2606b78db985d89eee
loudagnese <louis.dagnese@gmail.com>
2022-06-08 00:04:36 -0700

    add audit time method to recolor p elements based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,12 @@
 // Array of objects to hold tasks. Object Properties - Date; Time; Description
 var taskScheduler = [];
 
-// get today in moment day format
-var today = moment().format("YYYYMMDD");
-
-// loadTask method
+// loadTask method from localStorage
 var loadTasks = function () {
     // get tasks from localStorage
     taskScheduler = JSON.parse(localStorage.getItem("taskScheduler"));
 
+    // if nothing returns from localStorage create new array of objects, else set text from memory
     if (!taskScheduler) {
         taskScheduler = []
         for (let i = 8; i < 18; i++) {
@@ -18,31 +16,52 @@ var loadTasks = function () {
             }
             taskScheduler.push(tempObj)
         }
-        localStorage.setItem("taskScheduler",JSON.stringify(taskScheduler))
+        localStorage.setItem("taskScheduler", JSON.stringify(taskScheduler))
     } else {
         for (let i = 0; i < taskScheduler.length; i++) {
             $(`#${taskScheduler[i].id}.time-block p`).text(taskScheduler[i].text);
         };
     };
+
+    // Call auditTaskTime to recolor on all updates
+    auditTaskTime();
 };
 
-// saveTask method
-var saveTask = function (taskTimeID, taskText) {
+// saveTask method to save array to localstorage
+var saveTask = function () {
     localStorage.setItem("taskScheduler", JSON.stringify(taskScheduler))
 }
 
 // auditTask method
+var auditTaskTime = function () {
+    // get the current hour in 24 hour format to compare to time-block id
+    var currentHour = moment().format('HH');
 
-// Display the Current Date in the Jumbotron <p> Element
-$("#currentDay").text(`Today is ${moment().format("LL")}`)
+    // loop over all p element by id and set the background colors
+    for (let i = 8; i < 18; i++) {
+        if (i < currentHour) {
+            $(`#${i}.time-block p`)
+                .removeClass()
+                .addClass("col-6 description past");
+        } else if (i == currentHour) {
+            $(`#${i}.time-block p`)
+                .removeClass()
+                .addClass("col-6 description present");
+        } else {
+            $(`#${i}.time-block p`)
+                .removeClass()
+                .addClass("col-6 description future");
+        }
+    }
+};
 
 // on click event for time-blocks to change to input
-$(".time-block").on("click","p",function () {
+$(".time-block").on("click", "p", function () {
     // get the current text of the area
     var text = $(this)
         .text()
         .trim();
-    
+
     // replace p element with a new textarea
     var textInput = $("<textarea>").addClass("col-6 descripton").val(text);
     $(this).replaceWith(textInput);
@@ -52,30 +71,39 @@ $(".time-block").on("click","p",function () {
 });
 
 // on click of save button run saveTask
-$(".saveBtn").on("click",function (){
+$(".saveBtn").on("click", function () {
     var inputText = $(this)
         .prev()
         .val();
-    
+
     var id = $(this)
         .parent()
         .attr("id");
 
-    taskScheduler.forEach(function(obj) {
+    // replace textInput for object matching edited id in array
+    taskScheduler.forEach(function (obj) {
         if (obj.id == id) {
             obj.text = inputText;
         }
-    })
+    });
 
+    // push new taskScheduler Array to localStorage
     saveTask();
 
     // replace textarea with p element
-    var pEl = $("<p>").addClass("col-6 descripton")
-    $(this).prev().replaceWith(pEl)
+    var pEl = $("<p>").addClass("col-6 descripton");
+    $(this).prev().replaceWith(pEl);
 
+    // reload tasks from localStorage
     loadTasks();
-
 })
 
+// Display the Current Date in the Jumbotron <p> Element on refresh or load
+$("#currentDay").text(`Today is ${moment().format("LL")}`)
+
+// need a setInterval to check the time and run auditTimeTask every minute
+setInterval(auditTaskTime, 60000)
+
+// loadTasks on refresh and load
 loadTasks();
 
